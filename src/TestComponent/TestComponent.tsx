98a58bbd6cee3f5413233736daa9a96d137c68e9
Einar Alexander Eymundsson <einar.alexander.eymundsson@samskip.com>
2021-06-27 00:33:50 +0000

    Added Scss modules support

--- src/TestComponent/TestComponent.tsx
+++ src/TestComponent/TestComponent.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 import { TestComponentProps } from "./TestComponent.types";
 
-import "./TestComponent.scss";
+import styles from "./TestComponent.scss";
 
 const TestComponent: React.FC<TestComponentProps> = ({ theme }) => (
   <div
     data-testid="test-component"
-    className={`test-component test-component-${theme}`}
+    className={styles[`test-component test-component-${theme}`]}
   >
-    <h1 className="heading">I'm the test component</h1>
+    <h1 className={styles.heading}>I'm the test component</h1>
     <h2>Made with love by Harvey</h2>
   </div>
 );
